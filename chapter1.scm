33187dc1df9f7c62e44061295734ff10bd0e8ad6
Tomoki Yamaguchi <tyamagu2@gmail.com>
2016-05-20 22:15:00 +0900

    1.3 Formulating Abstractions with Higher-Order Procedures

--- chapter1.scm
+++ chapter1.scm
@@ -520,3 +520,349 @@
 
 ; Non-Carmichael numbers
 (miller-rabin-prime? (* 561 561) 100) ; #t
+
+
+;; 1.3  Formulating Abstractions with Higher-Order Procedures
+
+; Ex. 1.29
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (integral f a b n)
+  (define (even? x) (= (remainder x 2) 0))
+  (define (inc x) (+ x 1))
+  (define h (/ (- b a) n))
+  (define (term k)
+    (define (coefficient k)
+      (cond ((or (= k 0) (= k n)) 1)
+            ((even? k) 2)
+            (else 4)))
+    (* (coefficient k) (f (+ a (* k h)))))
+  (/ (* h (sum term 0 inc n)) 3))
+
+(define (cube x) (* x x x))
+(integral cube 0 1 100)  ; 1/4
+(integral cube 0 1 1000) ; 1/4
+
+; Ex. 1.30
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (a > b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+; Ex. 1.31 & Ex. 1.32
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+(define (product term a next b)
+  (define (product2 a b) (* a b))
+  (accumulate product2 1 term a next b))
+
+(define (factorial x)
+  (define (identity x) x)
+  (define (inc x) (+ x 1))
+  (product identity 1 inc x))
+
+(define (pi n)
+  (define (inc x) (+ x 1))
+  (define (even? x) (= (remainder x 2) 0))
+  (define (term x)
+    (cond ((= x 1)
+           (/ 2 3))
+          ((even? x)
+           (/ (+ x 2) (+ 1 x)))
+          (else (/ (+ x 1) (+ x 2)))))
+  (* 4 (product term 1 inc n)))
+
+; Ex. 1.33
+(define (filtered-accumulate combiner null-value term a next b filter)
+  (cond ((> a b)
+         null-value)
+        ((filter a)
+         (combiner (term a)
+                   (filtered-accumulate combiner null-value term (next a) next b filter)))
+        (else (filtered-accumulate combiner null-value term (next a) next b filter))))
+
+(define (smallest-divisor n)
+  (define (divides? a b) (= (remainder a b) 0))
+  (define (square n) (* n n))
+  (define (find-divisor n t)
+    (cond ((> (square t) n) n)
+          ((divides? n t) t)
+          (else (find-divisor n (+ t 1)))))
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+; a
+(define (sum-squared-primes a b)
+  (define (add a b) (+ a b))
+  (define (square x) (* x x))
+  (define (inc x) (+ x 1))
+  (filtered-accumulate add 0 square a inc b prime?))
+
+; b
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (sum-positive-relative-primes n)
+  (define (filter x)
+    (and (> x 0)
+         (= (gcd x n) 1)))
+  (filtered-accumulate
+   (lambda (a b) (+ a b))
+   0
+   (lambda (x) x)
+   1
+   (lambda (x) (+ x 1))
+   (- n 1)
+   filter))
+
+; Ex. 1.34
+(define (f g)
+  (g 2))
+; (f f)
+; (f 2)
+; (2 2)
+; => invalid application
+
+; Ex. 1.35
+; Let PHI be the golden ratio (1 + sqrt(5)) / 2.
+; 1 + 1 / PHI
+; = 1 + 2 / (1 + sqrt(5))
+; = (3 + sqrt(5)) / (1 + sqrt(5))
+; By multipying (1 - sqrt(5)) to both the denominator and the numerator, we have
+; (3 - 2 * sqrt(5) - 5) / (1 - 5)
+; = -2 * (1 + sqrt(5)) / -4
+; = (1 + sqrt(5)) / 2
+; = PHI
+; Therefore, the golden ratio is a fixed point of the transformation x -> 1 + 1 / x
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next-guess (f guess)))
+      (if (close-enough? guess next-guess)
+          next-guess
+          (try next-guess))))
+  (try first-guess))
+
+(define (golden-ratio)
+  (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
+
+; Ex. 1.36
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next-guess (f guess)))
+      (display guess)
+      (newline)
+      (if (close-enough? guess next-guess)
+          next-guess
+          (try next-guess))))
+  (try first-guess))
+
+; Takes about 40 steps
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 2)
+
+; Takes about 10 steps
+(define (y x) (/ (log 1000) ( log x)))
+(define (average x y) (/ (+ x y) 2))
+(fixed-point (lambda (x) (average x (y x))) 2)
+
+; Ex. 1.37
+(define (cont-frac n d k)
+  (define (rec c)
+    (if (> c k)
+        0
+        (/ (n k) (+ (d k) (rec (+ c 1))))))
+  (rec 1))
+
+(define (cont-frac n d k)
+  (define (iter c res)
+    (if (= c 0)
+        res
+        (iter (- c 1) (/ (n c) (+ (d c) res)))))
+  (iter k 0))
+
+(/ 1 (cont-frac (lambda (i) 1.0)
+                (lambda (i) 1.0)
+                12))
+; 1.6180...
+
+; Ex. 1.38
+(+ 2 (cont-frac (lambda (i) 1)
+                (lambda (i)
+                  (if (= (remainder i 3) 2)
+                      (* 2.0 (+ 1 (quotient i 3)))
+                      1))
+                10))
+
+; Ex. 1.39
+(define (tan-cf x k)
+  (cont-frac (lambda (i)
+               (if (= i 1)
+                   x
+                   (- (* x x))))
+             (lambda (i) (- (* 2 i) 1))
+             k))
+
+(define (average-damp f)
+  (define (average a b) (/ (+ a b) 2))
+  (lambda (x) (average x (f x))))
+
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+               1.0))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+               1.0))
+
+(define dx 0.00001)
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newton-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt x)
+  (newton-method (lambda (y) (- (square y) x))
+                 1.0))
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+(define (square x)
+  (fixed-point-of-transform (lambda (y) (/ x y))
+                            average-damp
+                            1.0))
+
+(define (square x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+                            newton-transform
+                            1.0))
+
+; Ex. 1.40
+(define (cubic a b c)
+  (define (cube x) (* x x x))
+  (define (square x) (* x x))
+  (lambda (x) (+ (cube x)
+                 (* a (square x))
+                 (* b x)
+                 c)))
+
+; Ex. 1.41
+(define (double f)
+  (lambda (x) (f (f x))))
+
+(define (inc x) (+ x 1))
+(((double (double double)) inc) 5) ; 21
+
+; Ex. 1.42
+(define (compose f g) (lambda (x) (f (g x))))
+
+(define (square x) (* x x))
+((compose square inc) 6)
+
+; Ex. 1.43
+(define (repeated f n)
+  (if (= n 1)
+      f
+      (compose f (repeated f (- n 1)))))
+
+; Ex. 1.44
+
+(define dx 0.00001)
+(define (smooth f)
+  (lambda (x)
+    (/ (+ (f (- x dx))
+          (f x)
+          (f (+ x dx)))
+       3)))
+
+(define (nth-fold-smooth f n)
+  (lambda (x)
+    (((repeated smooth n) f) x)))
+
+; Ex. 1.45
+(define (pow x n)
+  (if (= n 0)
+      1
+      (* x (pow x (- n 1)))))
+
+(define (test-nth-root n x rn)
+  (fixed-point ((repeated average-damp rn)
+                (lambda (y) (/ x (pow y (- n 1)))))
+               1.0))
+
+; # of repeat to converge:
+; sqrt      => 1
+; cube-root => 1
+; 4th-root  => 2
+; 5th-root  => 2
+; ...
+; 7th-root  => 2
+; 8th-root  => 3
+; ...
+; 15th-root => 3
+; 16th-root => 4
+;
+; => (floor (log n 2))
+
+(define (nth-root n x)
+  (fixed-point ((repeated average-damp (floor (log n 2)))
+                (lambda (y) (/ x (pow y (- n 1)))))
+               1.0))
+
+(nth-root 10 1024) ; 2.000001183010332
+
+; Ex. 1.46
+(define (iterative-improve good-enough? improve)
+  (lambda (guess)
+    (let ((new-guess (improve guess)))
+      (if (good-enough? guess new-guess)
+          new-guess
+          ((iterative-improve good-enough? improve) new-guess)))))
+
+(define (sqrt x)
+  ((iterative-improve (lambda (guess new-guess)
+                        (< (abs (- new-guess guess)) 0.00001))
+                      (lambda (guess)
+                        (average guess (/ x guess))))
+   1.0))
+
+(define (fixed-point f first-guess)
+  ((iterative-improve (lambda (guess new-guess)
+                       (< (abs (- new-guess guess)) 0.00001))
+                     f)
+   first-guess))
