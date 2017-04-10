132a9882f7a8474ef2215d992249d1738d8b182a
tyamagu2 <tyamagu2@gmail.com>
2017-04-11 00:16:29 +0900

    ~ Ex. 2.68

--- chapter2.scm
+++ chapter2.scm
@@ -1482,3 +1482,171 @@
 ;
 ; The number of steps performed by tree->list-2 is T(n)
 ; since each recursive call performes one cons operation.
+
+; Ex. 2.64
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+; a
+; partial-tree splits the first n elements of the given list into three parts;
+; the median element, the list of elements smaller than the median element
+; and the list of elements bigger than the median element.
+; partial-tree then creates a tree whose root is the median element
+; and whose left and right subtrees are the results of partial-tree applied to
+; the list of smaller and bigger elements.
+;
+;       5
+;      / \
+;     /   \
+;    1     9
+;     \   / \
+;      3 7   11
+
+; b
+; Theta (n)
+
+; Ex. 2.65
+(define (union-list l1 l2)
+  (cond ((null? l1) l2)
+        ((null? l2) l1)
+        (else (let ((e1 (car l1))
+                    (e2 (car l2)))
+                (cond ((= e1 e2) (cons e1 (union-list (cdr l1) (cdr l2))))
+                      ((< e1 e2) (cons e1 (union-list (cdr l1) l2)))
+                      (else (cons e2 (union-list l1 (cdr l2)))))))))
+
+(define (union-set s1 s2)
+  (let ((l1 (tree->list-2 s1))
+        (l2 (tree->list-2 s2)))
+    (list->tree (union-list l1 l2))))
+
+(define (intersection-list l1 l2)
+  (if (or (null? l1) (null? l2))
+      '()
+      (let ((e1 (car l1))
+            (e2 (car l2)))
+        (cond ((= e1 e2) (cons e1 (intersection-list (cdr l1) (cdr l2))))
+              ((< e1 e2) (intersection-list (cdr l1) l2))
+              (else (intersection-list l1 (cdr l2)))))))
+
+(define (intersection-set s1 s2)
+  (let ((l1 (tree->list-2 s1))
+        (l2 (tree->list-2 s2)))
+    (list->tree (intersection-list l1 l2))))
+
+; Ex. 2. 66
+(define key identity)
+(define (look-up given-key set-of-records)
+  (if (null? set-of-records)
+      #f
+      (let ((e (entry set-of-records)))
+        (cond ((= given-key (key e)) e)
+              ((< given-key (key e)) (look-up given-key (left-branch set-of-records)))
+              (else (look-up given-key (right-branch set-of-records)))))))
+
+; 2.3.4  Example: Huffman Encoding Trees
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+    (decode-1 bits tree))
+(define (choose-branch bit current-branch)
+  (cond ((= bit 0) (left-branch current-branch))
+        ((= bit 1) (right-branch current-branch))
+        (else (error "bad-bit -- CHOOSE_BRRANCH" bit))))
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)
+                               (cadr pair))
+                    (make-leaf-set (cdr pairs))))))
+
+; Ex. 2.67
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(decode sample-message sample-tree) ; => (A D A B B C A)
+
+; Ex. 2.68
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (member? e l)
+  (cond ((null? l) #f)
+        ((eq? e (car l)) #t)
+        (else (member? e (cdr l)))))
+
+(define (encode-symbol symbol tree)
+  (if (leaf? tree)
+      '()
+      (let ((left (left-branch tree))
+            (right (right-branch tree)))
+        (cond ((member? symbol (symbols left))
+               (cons 0 (encode-symbol symbol left)))
+              ((member? symbol (symbols right))
+               (cons 1 (encode-symbol symbol right)))
+              (else (error "bad-symbol -- encode-symbol" symbol))))))
+
+
+(encode (list 'A 'D 'A 'B 'B 'C 'A) sample-tree)
+; => (0 1 1 0 0 1 0 1 0 1 1 1 0)
+(encode (list 'T 'E 'S 'T) sample-tree)
+; => *** ERROR: bad-symbol -- encode-symbol T
